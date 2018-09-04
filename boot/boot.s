c279e4e4393cfdfbfdad561a09b848d7aa942f05
Vincent Hu <vincent@Vincent-iMac.local>
2018-09-04 22:05:29 +0800

    fix msg display in bootloader

--- boot/boot.s
+++ boot/boot.s
@@ -6,18 +6,19 @@
 org 0x7c00
 
 mov si, msg
-call PrintMsg
+call print_msg
 jmp $ ;Infinite loop, hang there.
 
-PrintMsg:
+print_msg:
     mov al, [si]
     inc si
-    cmp al, 0
+    cmp al, 0 ;Check if value in AL is zero (end of string)
     je exit
-    call PrintCharacter
-    jmp PrintMsg
+    call print_character 
+    jmp print_msg
+	ret
 
-PrintCharacter:
+print_character:
     mov ah, 0x0e ;BIOS to print a charater on screen.
     mov bh, 0x00
     mov bl, 0x07
@@ -25,12 +26,12 @@ PrintCharacter:
     int 0x10 ;Call video interrupt
     ret
 
-msg:
-   db "Loading system ..."  
-   db 0x0a
-
 exit:
     ret
+
+msg:
+   db "Loading system ..."  
+   db 0
 
 times 510 - ($ - $$) db 0 ;Fill the rest of sector with 0
 dw 0xaa55 ;Add boot signature at the end of bootloader
