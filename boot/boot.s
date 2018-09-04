46a8572270ae5a3f9e3ec014804b2bd79d81d477
BabyKnight <zju_hgh@163.com>
2018-09-04 09:59:00 +0000

    bootloader support print a message string

--- boot/boot.s
+++ boot/boot.s
@@ -5,16 +5,32 @@
 
 org 0x7c00
 
-mov al, 65
+mov si, msg
 call PrintMsg
 jmp $ ;Infinite loop, hang there.
 
 PrintMsg:
-    mov ah, 0x0e
+    mov al, [si]
+    inc si
+    cmp al, 0
+    je exit
+    call PrintCharacter
+    ret
+
+
+PrintCharacter:
+    mov ah, 0x0e ;BIOS to print a charater on screen.
     mov bh, 0x00
     mov bl, 0x07
+    ;mov bx, 15 ;set character color
+    int 0x10 ;Call video interrupt
+    ret
 
-    int 0x10
+msg:
+   db "Loading system ..."  
+   db 0x0a
+
+exit:
     ret
 
 times 510 - ($ - $$) db 0 ;Fill the rest of sector with 0
