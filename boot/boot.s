8a3dfeca4a8a597ae100cb107cad954fb0cbde2e
BabyKnight <zju_hgh@163.com>
2018-09-04 09:35:27 +0000

    bootloader support print 'A' then hanging there, change all Instructions to lowercase

--- boot/boot.s
+++ boot/boot.s
@@ -3,9 +3,19 @@
 ;
 ; boot.s is loaded at 0x7c00 by the bios-startup routines
 
-ORG 0x7C00
+org 0x7c00
 
-JMP $ ;Infinite loop, hang there.
+mov al, 65
+call PrintMsg
+jmp $ ;Infinite loop, hang there.
 
-TIMES 510 - ($ - $$) DB 0 ;Fill the rest of sector with 0
-DW 0xAA55 ;Add boot signature at the end of bootloader 
+PrintMsg:
+    mov ah, 0x0e
+    mov bh, 0x00
+    mov bl, 0x07
+
+    int 0x10
+    ret
+
+times 510 - ($ - $$) db 0 ;Fill the rest of sector with 0
+dw 0xaa55 ;Add boot signature at the end of bootloader
