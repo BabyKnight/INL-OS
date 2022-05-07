e67d0725f3ba9b2325536aad0b832fc682123090
Vincent Hu <vincent.guohua.hu@gmail.com>
2022-05-07 22:16:13 +0800

    Update bootesct, move itself to 0x90000 and jump to there

--- boot/boot.s
+++ boot/boot.s
@@ -1,6 +1,7 @@
 !
 !	boot.s            (C) 2018 Vinent Hu
-!	Intel style Assembly language (Compiled by NASM)
+!
+!	Intel style Assembly language
 !
 ! boot.s is loaded at 0x7c00 by the bios-startup routines, and moves
 ! itself out of the way to address 0x90000, and jump there.
@@ -8,6 +9,13 @@
 ! It then loads 'setup' directly after itself (0x90200), and the system
 ! at 0x10000, using BIOS interrupts.
 
+! SYSSIZE is the number of clicks (16bytes) to be loaded.
+! 0x3000 is 0x30000 bytes = 196kB, more than enough for current versions of this OS
+
+! Since the code of bootsect and setup will be move to the memory start from 0x90000,
+! so SYSSIZE should no large than 0x9000 (584KB)
+!
+SYSSIZE = 0x3000
 
 .globl begtext, begdata, begbss, endtext, enddata, endbss
 .text
@@ -34,7 +42,7 @@ start:
 	sub si, si
 	sub di, di
 	rep
-	movw            ! move bootsect itself to address 0x90000  (256 * 2 Bytes)
+	movw                ! move bootsect itself to address 0x90000  (256 * 2 Bytes)
 	jmpi go, INITSEG    ! jump to address 0x90000 and execute
 
 go:
@@ -44,36 +52,31 @@ go:
 	mov ss, ax
 	mov sp, #0xFF00     ! Top of the stack - ss:sp address is 0x9FF00
 
+	mov ah, #0x03       ! read cursor pos
+	xor bh, bh
+	int 0x10
 
+	mov cx, #24
+	mov bx, #0x0007
+	mov bp, #msg1
+	mov ax, #0x1301
+	int 0x10
 
-! ok, print loading message
-mov si, msg
-call print_msg
-jmp $ !Infinite loop, hang there.
+msg1:
+	.byte 13,10
+	.ascii "VNL OS  - Loading system ..."
+	.byte 13,10,13,10
 
-print_msg:
-    mov al, [si]
-    inc si
-    cmp al, 0 !Check if value in AL is zero (end of string)
-    je exit
-    call print_character 
-    jmp print_msg
-	ret
+! times 510 - ($ - $$) db 0 ! Fill the rest of sector with 0
+! dw 0xaa55 !Add boot signature at the end of bootloader
 
-print_character:
-    mov ah, 0x0e !BIOS to print a charater on screen.
-    mov bh, 0x00
-    mov bl, 0x07
-    !mov bx, 15 !set character color
-    int 0x10 !Call video interrupt
-    ret
+.org 510
+boot_flag:
+	.word 0xAA55
 
-exit:
-    ret
-
-msg:
-   db "Loading system ..."  
-   db 0
-
-times 510 - ($ - $$) db 0 !Fill the rest of sector with 0
-dw 0xaa55 !Add boot signature at the end of bootloader
+.text
+endtext:
+.data
+enddata:
+.bss
+endbss:
