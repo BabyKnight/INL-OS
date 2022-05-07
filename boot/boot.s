6735e655b3c9acd162b2049fe2bb38027f5a7ce8
Vincent Hu <vincent.guohua.hu@gmail.com>
2022-05-07 17:01:01 +0800

    refactor code for boot.s, move itself to 0x90000

--- boot/boot.s
+++ boot/boot.s
@@ -1,55 +1,71 @@
-;
-;	boot.s            (C) 2018 Vinent Hu
-;	Intel style Assembly language (Compiled by NASM)
-;
-; boot.s is loaded at 0x7c00 by the bios-startup routines, and moves
-; itself out of the way to address 0x90000, and jump there.
-;
-; It then loads 'setup' directly after itself (0x90200), and the system
-; at 0x10000, using BIOS interrupts.
+!
+!	boot.s            (C) 2018 Vinent Hu
+!	Intel style Assembly language (Compiled by NASM)
+!
+! boot.s is loaded at 0x7c00 by the bios-startup routines, and moves
+! itself out of the way to address 0x90000, and jump there.
+!
+! It then loads 'setup' directly after itself (0x90200), and the system
+! at 0x10000, using BIOS interrupts.
 
-;SYSSIZE = 0x30000
 
-;BOOTSEG = 0x7c00
-;INITSEG = 0x90000
-;SETUPSEG = 0x90200
-;SYSSEG = 0x10000
-;ENDSEG = SYSSEG + SYSSIZE 
+.globl begtext, begdata, begbss, endtext, enddata, endbss
+.text
+begtext:
+.data
+begdata:
+.bss
+begbss:
+.text
 
-org 0x7c00
+BOOTSEG  = 0x07c0            ! original address of boot sector
+INITSEG  = 0x9000            ! move bootsect here
+SETUPSEG = 0x9020            ! setup starts here
+SYSSEG   = 0x1000            ! system loaded at 0x10000 (65536)
+ENDSEG   = SYSSEG + SYSSIZE  ! where to stop loading
 
-; move itself to 0x90000
-;mov ax, 0x7c00
-;mov ds, ax
-;mov ax, 0x90000
-;mov es, ax
-;mov cx, 256
-;sub si, si
-;sub di, di
-;rep
-;movw
-;jmpi go 0x90000
+entry start
+start:
+	mov ax, #BOOTSEG
+	mov ds, ax
+	mov ax, #INITSEG
+	mov es, ax
+	mov cx, #256
+	sub si, si
+	sub di, di
+	rep
+	movw            ! move bootsect itself to address 0x90000  (256 * 2 Bytes)
+	jmpi go, INITSEG    ! jump to address 0x90000 and execute
 
-; ok, print loading message
+go:
+	mov ax, cs
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
+	mov sp, #0xFF00     ! Top of the stack - ss:sp address is 0x9FF00
+
+
+
+! ok, print loading message
 mov si, msg
 call print_msg
-jmp $ ;Infinite loop, hang there.
+jmp $ !Infinite loop, hang there.
 
 print_msg:
     mov al, [si]
     inc si
-    cmp al, 0 ;Check if value in AL is zero (end of string)
+    cmp al, 0 !Check if value in AL is zero (end of string)
     je exit
     call print_character 
     jmp print_msg
 	ret
 
 print_character:
-    mov ah, 0x0e ;BIOS to print a charater on screen.
+    mov ah, 0x0e !BIOS to print a charater on screen.
     mov bh, 0x00
     mov bl, 0x07
-    ;mov bx, 15 ;set character color
-    int 0x10 ;Call video interrupt
+    !mov bx, 15 !set character color
+    int 0x10 !Call video interrupt
     ret
 
 exit:
@@ -59,5 +75,5 @@ msg:
    db "Loading system ..."  
    db 0
 
-times 510 - ($ - $$) db 0 ;Fill the rest of sector with 0
-dw 0xaa55 ;Add boot signature at the end of bootloader
+times 510 - ($ - $$) db 0 !Fill the rest of sector with 0
+dw 0xaa55 !Add boot signature at the end of bootloader
